3e0bba025cb4129910b528f73e334e92f084555b
NYessengeldi <54642111+NYessengeldi@users.noreply.github.com>
2021-05-25 20:55:05 +0600

    Update Shannon-Fano.cpp
    copying probabilities to txt file

--- Shannon-Fano.cpp
+++ Shannon-Fano.cpp
@@ -5,24 +5,26 @@ using namespace std;
 int used[260];
 string encoded[260];
 vector<pair<int, char> > v;
+int nodeStep = 1;
+string compressed;
 
 struct node {
-    int id;
     string code;
     vector<pair<int, char> > prob;
     int tot = 0;
-};
+} tree[10000];
 
 vector<node> steps[100];
 
 void printChar(char c){
     if(c == ' ') cout << "Space";
     else if(c == '\n') cout << "Endline";
-    else if(c == '\t') cout << "Endline";
     else cout << c;
 }
 
-void build(int j, node x){
+void build(int v, int j, node x){
+    tree[v] = x;
+
     if(x.prob.size() == 1){
         if(x.code == "") encoded[x.prob[0].second] = x.code = "0";
         else encoded[x.prob[0].second] = x.code;
@@ -37,17 +39,17 @@ void build(int j, node x){
         else ff.push_back(x.prob[i]), res += x.prob[i].first;
     }
 
-    steps[j].push_back({x.id+1, x.code + '1', ss, x.tot - res});
-    steps[j].push_back({x.id+2, x.code + '0', ff, res});
-    build(j+1, {x.id+2, x.code + '1', ss, x.tot - res});
-    build(j+1, {x.id+4, x.code + '0', ff, res});
+    steps[j].push_back({x.code + '1', ss, x.tot - res});
+    steps[j].push_back({x.code + '0', ff, res});
+    build(v*2+1, j+1, {x.code + '1', ss, x.tot - res});
+    build(v*2, j+1, {x.code + '0', ff, res});
 }
 
 void printStep(int i){
     cout << "Step " << i << ".\n";
 
     for(int j = 0; j < steps[i].size(); j++){
-        cout << "Node" << steps[i][j].id << ": (" << steps[i][j].code << ") ";
+        cout << "Node" << nodeStep++ << ": (" << steps[i][j].code << ") ";
         for(int k = 0; k < steps[i][j].prob.size(); k++){
             printChar(steps[i][j].prob[k].second);
             if(k != steps[i][j].prob.size()-1) cout << ", ";
@@ -58,19 +60,21 @@ void printStep(int i){
 }
 
 int main(){
+    // ios_base :: sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    for(int i = 0; i < 260; i++){
+        used[i] = 0;
+    }
+    
     string s = "";
     string textchar;
 
     ifstream infile;
-    infile.open("Text.txt");
+    infile.open("txt/text.txt");
 
     while(getline(infile, textchar)){
         s += textchar;
     }
-    
-    for(int i = 0; i < 260; i++){
-        used[i] = 0;
-    }
+
     int n = s.size();
     for(int i = 0; i < n; i++){
         int x = s[i];
@@ -98,47 +102,38 @@ int main(){
     }
 
     // Task2
-    build(1, {0, "", v, n});
+    build(1, 1, {"", v, n});
     int ind = 1;
     while(steps[ind].size() > 0){
         printStep(ind);
         ind++;
     }
 
-    // Task3
-    cout << "symbol\t|\tprobability\t|\tcodeword\n";
-    for(int i = 0; i < v.size(); i++){
-        if(!check[v[i].second]){
-            printChar(v[i].second);
-            cout << "\t|\t" << 1.0*v[i].first/n << "\t\t|\t" << encoded[v[i].second] << endl;
-            check[v[i].second] = 1;
+    //writing to decompres.txt file
+    string to_file = "";
+
+    for(int i = 1; i < 10000; i++){
+        if(!tree[i].prob.empty()){
+            to_file += to_string(i) + ",";
+            for(int j = 0; j < tree[i].prob.size(); j++){
+                to_file.push_back(tree[i].prob[j].second);
+                to_file += " ";
+            }
+            to_file += "\n";
         }
     }
-    cout << endl;
+    ofstream MyFile("txt/algorithm.txt");
+    MyFile << to_file;
+    MyFile.close();
 
     // Task4
-    string compressed = "";
     for(int i = 0; i < n; i++){
         compressed += encoded[s[i]];
     }
     cout << compressed << endl << endl;
 
-    // Task5
-    int len = compressed.size();
-    int sum1 = 0, sum2 = 0;
-    for(int i = 0; i < n; i++){
-        sum1 += used[s[i]] * encoded[s[i]].size();
-        sum2 += used[s[i]];
-    }
-    cout << "Number of bits in the original text: " << n*8 << "bits\n";
-    cout << "Number of bits in the compressed text: " << len << "bits\n";
-    cout << "Compression ratio = " << fixed << setprecision(3) << 1.0*n*8/len <<  endl;
-    cout << "Average code length = " << fixed << setprecision(3) << 1.0*sum1/sum2 << endl;
-    cout << endl;
+    ofstream MyFile2("txt/encoded.txt");
+    MyFile2 << compressed;
+    MyFile2.close();
 
-    ofstream MyFile("new.txt");
-
-    MyFile << compressed;
-
-    MyFile.close();
 }
